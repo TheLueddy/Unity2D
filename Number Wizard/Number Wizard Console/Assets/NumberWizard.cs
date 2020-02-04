ae4b9fbec018099e71c5892f3bd60e9fa8d354be
TheLueddy <patrick.sequeira95@gmail.com>
2020-02-04 22:43:57 +0100

    - Add max/min variable and player input

--- Number Wizard/Number Wizard Console/Assets/NumberWizard.cs
+++ Number Wizard/Number Wizard Console/Assets/NumberWizard.cs
@@ -7,15 +7,23 @@ public class NumberWizard : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
+        int max = 1000;
+        int min = 1;
+
         Debug.Log("Welcome to Number Wizard!");
         Debug.Log("Pick a number, but don't tell me what it is...");
-        Debug.Log("The highest number you can pick is 1000.");
-        Debug.Log("The lowest number you can pick is 1.");
+        Debug.Log("The highest number you can pick is " + max + ".");
+        Debug.Log("The lowest number you can pick is " + min + ".");
+        Debug.Log("Tell me if your number is higher or lower than 500.");
+        Debug.Log("Push Up = Higher, Push Down = Lower, Push Enter = Correct number");
     }
-
+    
     // Update is called once per frame
     void Update()
     {
-        
+        if(Input.GetKeyDown(KeyCode.UpArrow))
+        {
+            Debug.Log("Up Arrow key was pressed!");
+        }
     }
 }
