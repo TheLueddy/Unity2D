3df5bd0e0ed6670a51bb1c7bde057dfd2aae81f8
TheLueddy <patrick.sequeira95@gmail.com>
2020-02-06 20:44:54 +0100

    - Initial creation of "Text101" project

---
+++ Text101/Assets/AdventureGame.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class AdventureGame : MonoBehaviour
+{
+    [SerializeField] Text textComponent;
+    [SerializeField] State startingState;
+
+    State currentState;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        currentState = startingState;
+        textComponent.text = currentState.GetStateStory();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        ManageState();
+    }
+
+    private void ManageState()
+    {
+        State[] nextStates = currentState.GetNextStates();
+
+        if (Input.GetKeyDown(KeyCode.Alpha1))
+        {
+            currentState = nextStates[0];
+        }
+        else if (Input.GetKeyDown(KeyCode.Alpha2))
+        {
+            currentState = nextStates[1];
+        }
+        else if (Input.GetKeyDown(KeyCode.Alpha3))
+        {
+            currentState = nextStates[2];
+        }
+
+        textComponent.text = currentState.GetStateStory();
+    }
+}
---
+++ Text101/Assets/State.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(menuName = "State")]
+public class State : ScriptableObject
+{
+    //14 = min size of text area
+    //10 = how many lines shown before scrolling starts
+    [TextArea(10,14)] [SerializeField] string storyText;
+    [SerializeField] State[] nextStates;
+
+    public string GetStateStory()
+    {
+        return storyText;
+    }
+
+    public State[] GetNextStates()
+    {
+        return nextStates;
+    }
+}
