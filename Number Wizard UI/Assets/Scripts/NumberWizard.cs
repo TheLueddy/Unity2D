ea30df336a0ecd2dcbf39e3d2a7606b019802034
TheLueddy <patrick.sequeira95@gmail.com>
2020-02-09 10:41:41 +0100

    - Finished "Number Wizard UI" project

---
+++ Number Wizard UI/Assets/Scripts/NumberWizard.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class NumberWizard : MonoBehaviour
+{
+    [SerializeField]int max;
+    [SerializeField]int min;
+    [SerializeField] Text guessText;
+
+
+    int guess;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        StartGame();
+    }
+
+    public void OnPressHigher()
+    {
+        min = guess;
+        NewGuess();
+    }
+
+    public void OnPressLower()
+    {
+        max = guess;
+        NewGuess();
+    }
+
+    void StartGame()
+    {
+        NewGuess();
+        max = max + 1;
+    }
+
+    void NewGuess()
+    {
+        guess = Random.Range(min, max);
+        guessText.text = guess.ToString();
+    }
+}
