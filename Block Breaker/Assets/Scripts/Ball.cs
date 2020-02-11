57bc3d70b815841e74e4f9b41a7798a9c7163bf0
Patrick Sequeira <patrick.sequeira95@gmail.com>
2020-02-11 20:47:42 +0100

    - Continued "Ball breaker"-project

---
+++ Block Breaker/Assets/Scripts/Ball.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ball : MonoBehaviour
+{
+    //Configuration parameters
+    [SerializeField] Paddle paddle;
+
+    //state
+    Vector2 paddleToBallVector;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        paddleToBallVector = transform.position - paddle.transform.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position = new Vector2(paddle.transform.position.x, paddle.transform.position.y + paddleToBallVector.y);
+    }
+}
