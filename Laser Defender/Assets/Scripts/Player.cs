0e4ac9541be735164eb2a4bf9cf5af8a6bf80e8c
Patrick Sequeira <patrick.sequeira95@gmail.com>
2020-04-07 21:22:55 +0200

    - Control Player movement in Laser Defender project

---
+++ Laser Defender/Assets/Scripts/Player.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Xml.Schema;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    // Configuration parameters
+    [SerializeField] float moveSpeed = 15f;
+    [SerializeField] float padding = 1f;
+    float xMin;
+    float xMax;
+    float yMin;
+    float yMax;
+    
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        SetMovementBoundaries();
+    }
+
+    //Set boundaries for position of player using ViewportToWorldPoint(). Independent of camera size!
+    private void SetMovementBoundaries()
+    {
+        Camera gameCamera = Camera.main;
+        xMin = gameCamera.ViewportToWorldPoint(new Vector3(0, 0, 0)).x + padding;
+        xMax = gameCamera.ViewportToWorldPoint(new Vector3(1, 0, 0)).x - padding;
+
+        yMin = gameCamera.ViewportToWorldPoint(new Vector3(0, 0, 0)).y + padding;
+        yMax = gameCamera.ViewportToWorldPoint(new Vector3(0, 1, 0)).y - padding;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Move();
+    }
+
+    private void Move()
+    {
+        //Multiplying with Time.deltaTima makes movement framerate independent!
+        var deltaX = Input.GetAxis("Horizontal") * Time.deltaTime * moveSpeed;
+        var newXPos = transform.position.x + deltaX;
+        newXPos = Mathf.Clamp(newXPos, xMin, xMax);
+
+        var deltaY = Input.GetAxis("Vertical") * Time.deltaTime * moveSpeed;
+        var newYPos = transform.position.y + deltaY;
+        newYPos = Mathf.Clamp(newYPos, yMin, yMax);
+
+        transform.position = new Vector2(newXPos, newYPos);
+    }
+}
