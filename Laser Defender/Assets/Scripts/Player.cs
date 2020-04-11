501db0f768c6acec9ccca4be5e0352f906de7bf0
Patrick Sequeira <patrick.sequeira95@gmail.com>
2020-04-11 17:30:24 +0200

    - Continued work on Laser Defender

--- Laser Defender/Assets/Scripts/Player.cs
+++ Laser Defender/Assets/Scripts/Player.cs
@@ -9,6 +9,12 @@ public class Player : MonoBehaviour
     // Configuration parameters
     [SerializeField] float moveSpeed = 15f;
     [SerializeField] float padding = 1f;
+    [SerializeField] GameObject laserPrefab;
+    [SerializeField] float laserSpeed= 10f;
+    [SerializeField] float projectileFiringPeriod = 0.1f;
+
+    Coroutine firingCoroutine;
+
     float xMin;
     float xMax;
     float yMin;
@@ -21,7 +27,36 @@ public class Player : MonoBehaviour
         SetMovementBoundaries();
     }
 
-    //Set boundaries for position of player using ViewportToWorldPoint(). Independent of camera size!
+    // Update is called once per frame
+    void Update()
+    {
+        Move();
+        Fire();
+    }
+
+    private void Fire()
+    {
+        if (Input.GetButtonDown("Fire1"))
+        {
+            firingCoroutine = StartCoroutine(FireContinuously());
+        }
+        if (Input.GetButtonUp("Fire1"))
+        {
+            StopCoroutine(firingCoroutine);
+        }
+    }
+
+    IEnumerator FireContinuously()
+    {
+        while (true)
+        {
+            GameObject laser = Instantiate(laserPrefab, transform.position, Quaternion.identity) as GameObject;
+            laser.GetComponent<Rigidbody2D>().velocity = new Vector2(0, laserSpeed);
+            yield return new WaitForSeconds(projectileFiringPeriod);
+        }
+    }
+
+    // Set boundaries for position of player using ViewportToWorldPoint(). Independent of camera size!
     private void SetMovementBoundaries()
     {
         Camera gameCamera = Camera.main;
@@ -32,15 +67,9 @@ public class Player : MonoBehaviour
         yMax = gameCamera.ViewportToWorldPoint(new Vector3(0, 1, 0)).y - padding;
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        Move();
-    }
-
     private void Move()
     {
-        //Multiplying with Time.deltaTima makes movement framerate independent!
+        // Multiplying with Time.deltaTima makes movement framerate independent!
         var deltaX = Input.GetAxis("Horizontal") * Time.deltaTime * moveSpeed;
         var newXPos = transform.position.x + deltaX;
         newXPos = Mathf.Clamp(newXPos, xMin, xMax);
